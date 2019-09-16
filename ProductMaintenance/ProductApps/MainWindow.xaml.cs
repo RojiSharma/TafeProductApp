9aeaa408bc6e56124dd8c798ae854533cfa8a138
Rojina Sharma <rojina.sharma@student.tafesa.edu.au>
2019-09-16 16:33:33 +0930

    "Commit the branch feat_GST!.2"

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 TotalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 wrapChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
+                GSTChargeTextBlock.Text = Convert.ToString((cProduct.TotalPayment + 25 + 5) * 1.1m);
             }
             catch (FormatException)
             {
